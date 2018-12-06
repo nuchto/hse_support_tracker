4c9b272e2893fdc776c4a288602971e93cdd4ae4
nuchto <evs1801@yandex.ru>
2018-12-07 00:48:25 +0300

    Revert "Init commit"
    This reverts commit 43fbfeaf839b5c377379c1c36f99125aadcde454.

--- app/models/ticket.rb
+++
@@ -1,5 +0,0 @@
-class Ticket < ApplicationRecord
-  belongs_to :reporter, class_name: "User", foreign_key: :reporter_id
-  # belongs_to :assigner, class_name: "User", foreign_key: :assigner_id
-  # belongs_to :assignee, class_name: "User", foreign_key: :assignee_id
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  resources :tickets do
-    member do
-     get 'new_assignment'
-     post 'create_assignment'
-   end
-  end
-  devise_for :users
-  root 'tickets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
